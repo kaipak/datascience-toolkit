b1a20c6e63e9f5cb36a11e9cf986fd4f605faca6
Kai Pak <kai@kaipak.org>
2019-12-04 16:51:04 -0800

    - Have get_distances function return an array of distances instead of entire dataframe.

--- src/clustering.py
+++ src/clustering.py
@@ -4,6 +4,36 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+from sklearn.cluster import KMeans
+
+
+def eval_KMeans(df: pd.DataFrame, columns: list=None, clust_min: int=1,
+                clust_max: int=4):
+    """ Take a dataframe and show diags on finding right number of clusters
+    that minimizes distortion.
+
+    Args:
+        df: Input dataframe
+        columns: Which columns to use for clustering. If none given, then 
+            default to using all columns.
+        clust_min: 
+    Returns:
+        void? 
+
+    """
+    if columns is None:
+        columns = df.columns
+
+    num_clusters = [int(n) for n in range(clust_min, clust_max + 1)]
+    mean_distances = []
+
+    for n in num_clusters:
+        cluster = KMeans(n_clusters=n)
+        cluster.fit(df[columns])
+
 
 
 def get_distances(df: pd.DataFrame, labels: np.ndarray, centers: np.ndarray,
@@ -11,21 +41,19 @@ def get_distances(df: pd.DataFrame, labels: np.ndarray, centers: np.ndarray,
     """Compute centers to clusters for each observation
 
     Args:
-        df_in: Input matrix to generate clusters
+        df: Input dataframe or matrix to generate clusters
         labels: Returned matrix of labels from clustering algorithm.
             This is typically generated as cluster.cluster_centers_
         centers: array of center coordinates that K_means generated
         metric: Which norm to use when computing distance
 
     """
-    df_ret = df.copy()
-    clusters = centers.shape[1]
 
-    df_ret['dist_to_cc'] = [
+    distances = [
         np.linalg.norm(centers[label] - df.loc[i, :])
         for i, label in enumerate(labels)
     ]
-    return df_ret
+    return distances
 
 
 def compute_distortion(df: pd.DataFrame):
